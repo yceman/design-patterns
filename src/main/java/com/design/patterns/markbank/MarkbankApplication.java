11dfe92434fbf3d0b436f1badc3d1f6009788724
Marcos Morais <marcosmorais.v10@gmail.com>
2025-03-25 17:49:14 -0300

    Spring Boot Project added

---
+++ src/main/java/com/design/patterns/markbank/MarkbankApplication.java
@@ -0,0 +1,13 @@
+package com.design.patterns.markbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkbankApplication.class, args);
+	}
+
+}
